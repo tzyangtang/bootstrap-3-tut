5f412fd389c67c975ac7d998cbe8dda528f979c2
Darren Tang <tty2191@gmail.com>
2014-01-31 16:13:47 -0500

    updates with angularJS

---
+++ app/app.js
@@ -0,0 +1,43 @@
+/// <reference path="../Scripts/angular-1.1.4.js" />
+
+/*#######################################################################
+  
+  Dan Wahlin
+  http://twitter.com/DanWahlin
+  http://weblogs.asp.net/dwahlin
+  http://pluralsight.com/training/Authors/Details/dan-wahlin
+
+  Normally like to break AngularJS apps into the following folder structure
+  at a minimum:
+
+  /app
+      /controllers      
+      /directives
+      /services
+      /partials
+      /views
+
+  #######################################################################*/
+
+var app = angular.module('mainApp', ['ngRoute']);
+
+//This configures the routes and associates each route with a view and a controller
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/',
+            {
+                controller: 'SingleController',
+                templateUrl: '/app/partials/main.html'
+            })
+        //Define a route that has a route parameter in it (:customerID)
+        .when('/article',
+            {
+                controller: 'SingleController',
+                templateUrl: '/app/partials/article.html'
+            })
+        .otherwise({ redirectTo: '/' });
+});
+
+
+
+
---
+++ app/controllers/controllers.js
@@ -0,0 +1,22 @@
+/*#######################################################################
+  
+  Dan Wahlin
+  http://twitter.com/DanWahlin
+  http://weblogs.asp.net/dwahlin
+  http://pluralsight.com/training/Authors/Details/dan-wahlin
+
+  Normally like the break AngularJS controllers into separate files.
+  Kept them together here since they're small and it's easier to look through them.
+  example. 
+
+  #######################################################################*/
+
+app.controller('SingleController', function ($scope) {
+
+    //I like to have an init() for controllers that need to perform some initialization. Keeps things in
+    //one place...not required though especially in the simple example below
+    init();
+
+    function init() {
+    }
+});
